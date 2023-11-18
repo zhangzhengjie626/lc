bf308c5a2ef5edfa543b02ffb9b1304f81d02a59
zhangzhengjie <1051557122@qq.com>
2023-11-18 16:41:22 +0800

    26. 删除有序数组中的重复项-笨办法&两次过

---
+++ src/test/java/removeDuplicates26/SolutionTest.java
@@ -0,0 +1,12 @@
+package removeDuplicates26;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void removeDuplicates() {
+        Solution solution = new Solution();
+        System.out.println(solution.removeDuplicates(new int[]{1,1,2}));
+    }
+}
