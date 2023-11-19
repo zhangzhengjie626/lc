936e68f2ef05f9f8e4b68fc7afcc4e66ecf4cb95
zhangzhengjie <1051557122@qq.com>
2023-11-20 07:19:44 +0800

    189. 轮转数组-二次过

---
+++ src/test/java/rotate189/SolutionTest.java
@@ -0,0 +1,17 @@
+package rotate189;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+
+public class SolutionTest {
+
+    @Test
+    public void rotate() {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.rotate(new int[]{1, 2, 3, 4, 5, 6, 7}, 3)));
+        System.out.println(Arrays.toString(solution.rotate(new int[]{-1,-100,3,99}, 2)));
+        System.out.println(Arrays.toString(solution.rotate(new int[]{1,2}, 3)));
+    }
+}
