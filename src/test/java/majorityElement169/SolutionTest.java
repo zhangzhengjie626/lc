7b9761686109ba88b74bae0472f77779fe10ef0d
zhangzhengjie <1051557122@qq.com>
2023-11-19 08:11:32 +0800

    169.多数元素-笨办法

---
+++ src/test/java/majorityElement169/SolutionTest.java
@@ -0,0 +1,14 @@
+package majorityElement169;
+
+import org.junit.Test;
+
+
+public class SolutionTest {
+
+    @Test
+    public void majorityElement() {
+        Solution solution = new Solution();
+        System.out.println(solution.majorityElement(new int[]{3,2,3}));
+        System.out.println(solution.majorityElement(new int[]{2,2,1,1,1,2,2}));
+    }
+}
