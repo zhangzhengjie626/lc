a684788c3ac2ab0e61bc6a0e5f894b4b37c37a73
zhangzhengjie <1051557122@qq.com>
2023-11-23 07:54:07 +0800

    58. 最后一个单词的长度-看错题目了其实一次过

--- src/main/java/lengthOfLastWord58/Solution.java
+++ src/main/java/lengthOfLastWord58/Solution.java
@@ -6,6 +6,7 @@ package lengthOfLastWord58;
 public class Solution {
     public int lengthOfLastWord(String s) {
         //我的思路：从前往后遇到空格重新记录长度，并判断是否最长
+        //晕了，看错题目了，还做了两次，sb了
 //        int ans = 0;
 //        int curWordLength = 0;
 //        for (int i = 0; i < s.length(); i++) {
@@ -20,23 +21,36 @@ public class Solution {
 //        return ans;
 
         //从短到长，每个单词比较是否被包含的话，就是O(n^2)了，估计是不行的，试一下吧，做不出来，拉倒
+//        int ans = 0;
+//        String[] s1 = s.trim().split(" ");
+//        for (int i = 0; i < s1.length; i++) {
+//            for (int j = 0; j < s1.length; j++) {
+//                if (j != i) {
+//                    //先把包含其他单词的置空
+//                    if (!s1[i].isEmpty() && !s1[j].isEmpty() && s1[i].contains(s1[j])) {
+//                        s1[i] = "";
+//                        break;
+//                    }
+//                }
+//            }
+//        }
+//        for (int i = 0; i < s1.length; i++) {
+//            ans = Math.max(s1[i].length(), ans);
+//        }
+//
+//        return ans;
+//    }
         int ans = 0;
-        String[] s1 = s.trim().split(" ");
-        for (int i = 0; i < s1.length; i++) {
-            for (int j = 0; j < s1.length; j++) {
-                if (j != i) {
-                    //先把包含其他单词的置空
-                    if (!s1[i].isEmpty() && !s1[j].isEmpty() && s1[i].contains(s1[j])) {
-                        s1[i] = "";
-                        break;
-                    }
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) !=' '){
+                ans++;
+            } else {
+                if(ans != 0){
+                    break;
                 }
             }
-        }
-        for (int i = 0; i < s1.length; i++) {
-            ans = Math.max(s1[i].length(), ans);
-        }
 
+        }
         return ans;
     }
 }
