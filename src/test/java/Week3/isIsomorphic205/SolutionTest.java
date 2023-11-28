aa2a0a8031daacae205cfc6cbd42999d0794a5fd
zhangzhengjie <1051557122@qq.com>
2023-11-29 07:15:34 +0800

    205.同构字符串-寄1

---
+++ src/test/java/Week3/isIsomorphic205/SolutionTest.java
@@ -0,0 +1,15 @@
+package Week3.isIsomorphic205;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void isIsomorphic() {
+        Solution solution = new Solution();
+        System.out.println(solution.isIsomorphic("egg", "add"));
+        System.out.println(solution.isIsomorphic("foo", "bar"));
+        System.out.println(solution.isIsomorphic("paper", "title"));
+        System.out.println(solution.isIsomorphic("badc", "baba"));
+    }
+}
