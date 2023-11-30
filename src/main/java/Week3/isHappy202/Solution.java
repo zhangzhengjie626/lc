fd71f0990c4c020de4f5027ce925b7ec4766a920
zhangzhengjie <1051557122@qq.com>
2023-12-01 05:33:31 +0800

    219.存在重复元素II-一次过

---
+++ src/main/java/Week3/isHappy202/Solution.java
@@ -0,0 +1,10 @@
+package Week3.isHappy202;
+
+/**
+ * 202.快乐数
+ */
+public class Solution {
+    public boolean isHappy(int n) {
+        return true;
+    }
+}
---
+++ src/test/java/Week3/containsNearbyDuplicate/SolutionTest.java
@@ -0,0 +1,14 @@
+package Week3.containsNearbyDuplicate;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void containsNearbyDuplicate() {
+        Solution solution = new Solution();
+        System.out.println(solution.containsNearbyDuplicate(new int[]{1, 2, 3, 1}, 3));
+        System.out.println(solution.containsNearbyDuplicate(new int[]{1, 0, 1, 1}, 1));
+        System.out.println(solution.containsNearbyDuplicate(new int[]{1, 2, 3, 1, 2, 3}, 2));
+    }
+}
