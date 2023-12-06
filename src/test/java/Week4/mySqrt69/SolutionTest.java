671a079738c2ca36ca4919dadcc5d0a2da95a81d
zhangzhengjie <1051557122@qq.com>
2023-12-06 08:13:18 +0800

    69.x的平方根-寄2

---
+++ src/test/java/Week4/mySqrt69/SolutionTest.java
@@ -0,0 +1,14 @@
+package Week4.mySqrt69;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void mySqrt() {
+        Solution solution = new Solution();
+        System.out.println(solution.mySqrt(4));
+        System.out.println(solution.mySqrt(8));
+        System.out.println(solution.mySqrt(2147395600));
+    }
+}
