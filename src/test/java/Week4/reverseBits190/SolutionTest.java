1d4c5fe7775b8fb2432e37f415c5a0bf64ce68e1
zhangzhengjie <1051557122@qq.com>
2023-12-07 07:14:24 +0800

    190.颠倒二进制位-看题解

---
+++ src/test/java/Week4/reverseBits190/SolutionTest.java
@@ -0,0 +1,12 @@
+package Week4.reverseBits190;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void reverseBits() {
+        Solution solution = new Solution();
+        System.out.println(solution.reverseBits(43261596));
+    }
+}
