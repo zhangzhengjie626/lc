af72c156ebcabf20fdea76b22b37612e99a75bea
zhangzhengjie <1051557122@qq.com>
2023-12-08 06:59:08 +0800

    20231208-5题

---
+++ src/main/java/Week4/searchInsert35/Solution.java
@@ -0,0 +1,21 @@
+package Week4.searchInsert35;
+
+/**
+ * 35.搜索插入位置
+ */
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right + 1;
+    }
+}
