ca523e87c209b4c9b843517e1504c8d839fe4ed4
zhangzhengjie626 <1051557122@qq.com>
2024-01-06 06:54:43 +0800

    77.组合-回溯问题

---
+++ src/test/java/Week5/combine77/SolutionTest.java
@@ -0,0 +1,14 @@
+package Week5.combine77;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void combine() {
+        Solution solution = new Solution();
+        System.out.println(solution.combine(4,2));
+    }
+}
