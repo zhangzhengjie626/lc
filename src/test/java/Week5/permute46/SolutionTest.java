7b1eb49108e4a947ec699965c443d6f440bb0376
zhangzhengjie626 <1051557122@qq.com>
2024-01-06 08:28:44 +0800

    46.全排列-一次过但效率低

---
+++ src/test/java/Week5/permute46/SolutionTest.java
@@ -0,0 +1,14 @@
+package Week5.permute46;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void permute() {
+        Solution solution = new Solution();
+        System.out.println(solution.permute(new int[]{1, 2, 3}));
+    }
+}
