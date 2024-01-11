7a9f07c3648d3f12e057e1550ea2b2e7d64617bf
zhangzhengjie626 <1051557122@qq.com>
2024-01-12 07:16:26 +0800

    150.逆波兰表达式求值

---
+++ src/main/java/Week6/evalRPN150/Solution.java
@@ -0,0 +1,39 @@
+package Week6.evalRPN150;
+
+import java.util.Stack;
+
+/**
+ * https://leetcode.cn/problems/evaluate-reverse-polish-notation/?envType=study-plan-v2&envId=top-interview-150
+ */
+public class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<String> s = new Stack<>();
+        for (String str : tokens) {
+            if (str.equals("+")) {
+                String op1 = s.pop();
+                String op2 = s.pop();
+                int res = Integer.parseInt(op2) + Integer.parseInt(op1);
+                s.push(String.valueOf(res));
+            } else if (str.equals("-")) {
+                String op1 = s.pop();
+                String op2 = s.pop();
+                int res = Integer.parseInt(op2) - Integer.parseInt(op1);
+                s.push(String.valueOf(res));
+            } else if (str.equals("*")) {
+                String op1 = s.pop();
+                String op2 = s.pop();
+                int res = Integer.parseInt(op2) * Integer.parseInt(op1);
+                s.push(String.valueOf(res));
+            } else if (str.equals("/")) {
+                String op1 = s.pop();
+                String op2 = s.pop();
+                int res = Integer.parseInt(op2) / Integer.parseInt(op1);
+                s.push(String.valueOf(res));
+            } else {
+                s.push(str);
+            }
+        }
+        return Integer.parseInt(s.peek());
+    }
+}
+//注意运算数的顺序，第一个数在第二个数后边
