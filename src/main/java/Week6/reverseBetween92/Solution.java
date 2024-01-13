9cafeb72a460cc8c665634c73d4d46a8af205ab3
zhangzhengjie626 <1051557122@qq.com>
2024-01-13 08:10:24 +0800

    92.反转链表II-end的处理，中间pre和start的更新

---
+++ src/main/java/Week6/reverseBetween92/Solution.java
@@ -0,0 +1,43 @@
+package Week6.reverseBetween92;
+
+import Common.ListNode;
+
+/**
+ * https://leetcode.cn/problems/reverse-linked-list-ii/?envType=study-plan-v2&envId=top-interview-150
+ */
+public class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode newHead = new ListNode();
+        newHead.next = head;
+
+        ListNode startPre = newHead;
+        ListNode start = startPre.next;
+        while (left != 1) {
+            startPre = startPre.next;
+            start = start.next;
+            left--;
+        }
+
+        ListNode end = newHead;
+        while (right != 0) {
+            end = end.next;
+            right--;
+        }
+        ListNode endNext = end.next;
+
+        ListNode pre = endNext;
+        ListNode newNext;
+        while (start != end) {
+            newNext = start.next;
+            start.next = pre;
+            pre = start;
+            start = newNext;
+
+
+        }
+        //end的处理
+        end.next = pre;
+        startPre.next = end;
+        return newHead.next;
+    }
+}
---
+++ src/test/java/Week6/reverseBetween92/SolutionTest.java
@@ -0,0 +1,19 @@
+package Week6.reverseBetween92;
+
+import Common.ListNode;
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void reverseBetween() {
+        ListNode tmp = new ListNode(1,
+                new ListNode(2,
+                        new ListNode(3,
+                                new ListNode(4,
+                                        new ListNode(5, null)))));
+
+        Solution solution = new Solution();
+        solution.reverseBetween(tmp, 2, 4);
+    }
+}
