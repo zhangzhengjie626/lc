879e9ffa5af758215f27607dce925903924c1e3f
zhangzhengjie626 <1051557122@qq.com>
2024-01-13 10:36:59 +0800

    98.验证二叉搜索树

---
+++ src/test/java/Week6/isValidBST98/SolutionTest.java
@@ -0,0 +1,21 @@
+package Week6.isValidBST98;
+
+import Common.TreeNode;
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void isValidBST() {
+        TreeNode treeNode = new TreeNode(5,
+                new TreeNode(1, null, null),
+                new TreeNode(4, new TreeNode(3, null, null), new TreeNode(6, null, null)));
+        Solution solution = new Solution();
+
+        TreeNode t1 = new TreeNode(2,
+                new TreeNode(1, null, null),
+                new TreeNode(3, null, null));
+
+        solution.isValidBST(t1);
+    }
+}
