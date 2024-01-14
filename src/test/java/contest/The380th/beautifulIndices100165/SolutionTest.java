89bbc9e9ae46480639c996881387288a12c4a51b
zhangzhengjie626 <1051557122@qq.com>
2024-01-14 12:46:30 +0800

    weekly-contest-380

---
+++ src/test/java/contest/The380th/beautifulIndices100165/SolutionTest.java
@@ -0,0 +1,14 @@
+package contest.The380th.beautifulIndices100165;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void beautifulIndices() {
+        Solution solution = new Solution();
+        System.out.println(solution.beautifulIndices("isawsquirrelnearmysquirrelhouseohmy", "my", "squirrel", 15));
+        System.out.println(solution.beautifulIndices("abcd", "a", "a", 4));
+        System.out.println(solution.beautifulIndices("eueuau", "u", "e", 3));
+    }
+}
---
+++ src/test/java/contest/The380th/findMaximumNumber100160/SolutionTest.java
@@ -0,0 +1,14 @@
+package contest.The380th.findMaximumNumber100160;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void findMaximumNumber() {
+        Solution solution = new Solution();
+        System.out.println(solution.findMaximumNumber(9, 1));
+        System.out.println(solution.findMaximumNumber(7, 2));
+        System.out.println(solution.findMaximumNumber(3278539330613L, 5));
+    }
+}
