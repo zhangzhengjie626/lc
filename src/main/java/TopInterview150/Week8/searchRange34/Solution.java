7f3707158147089a7d4143dc255340e0c024c641
zhangzhengjie626 <1051557122@qq.com>
2024-01-28 06:33:41 +0800

    34.在排序数组中查找元素的第一个和最后一个位置

---
+++ src/main/java/TopInterview150/Week8/searchRange34/Solution.java
@@ -0,0 +1,38 @@
+package TopInterview150.Week8.searchRange34;
+
+/**
+ * https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/?envType=study-plan-v2&envId=top-interview-150
+ */
+public class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] res = new int[]{-1, -1};
+        if (nums.length == 0) {
+            return res;
+        }
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (left != nums.length && nums[left] == target) {
+            res[0] = left;
+
+            left = 0;
+            right = nums.length - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] <= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            res[1] = left - 1;
+        }
+        return res;
+    }
+}
