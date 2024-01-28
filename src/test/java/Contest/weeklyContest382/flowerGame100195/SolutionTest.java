8a110faf0f88aae78de8372ea6286e7bbf9a9a89
zhangzhengjie626 <1051557122@qq.com>
2024-01-28 13:49:21 +0800

    weekly-contest-382 3题

---
+++ src/test/java/Contest/weeklyContest382/flowerGame100195/SolutionTest.java
@@ -0,0 +1,16 @@
+package Contest.weeklyContest382.flowerGame100195;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void flowerGame() {
+        Solution solution = new Solution();
+        System.out.println(solution.flowerGame(3, 2));
+        System.out.println(solution.flowerGame(1, 1));
+        System.out.println(solution.flowerGame(4, 2));
+
+    }
+}
+//4 ,2 res 4
